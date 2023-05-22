15442ae625e865627d1c80a91b49aea29032eab0
mauk14 <mnd33599@gmail.com>
2023-05-22 21:45:56 +0600

    did slides 4 7 11

--- services/contact/cmd/app/main.go
+++ services/contact/cmd/app/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"ass3adv2/pkg/store/postgres"
+	"fmt"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	_, err := postgres.OpenDb("localhost", "5432", "postgres", "123456", "ass3adv2")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Success connect")
 }
